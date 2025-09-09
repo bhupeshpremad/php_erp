333bd27435cf717604d4577547d5619df5ac139f
bhupesh <premad.bhupesh@gmail.com>
2025-09-09 16:02:33 +0530

    ULTIMATE FIX: Final complete solution for purchase module

--- modules/purchase/add.php
+++ modules/purchase/add.php
@@ -203,7 +203,7 @@ try {
 <script src="js/purchase-editable.js?v=<?php echo time(); ?>"></script>
 <script src="js/bulk-operations.js?v=<?php echo time(); ?>"></script>
 <script src="fix_individual_save.js?v=<?php echo time(); ?>"></script>
-<script src="complete_module_fix.js?v=<?php echo time(); ?>"></script>
+<script src="ultimate_fix.js?v=<?php echo time(); ?>"></script>
 
 <script>
 // Force cache refresh - Version: <?php echo date('Y-m-d H:i:s'); ?>
